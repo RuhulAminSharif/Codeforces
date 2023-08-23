98bb922ce4337c2a9073041de01fd8f7d8a8a107
RuhulAminSharif <sharif.cse.ras@gmail.com>
2023-08-23 17:54:22 +0600

    1742A added

---
+++ Codeforces/1742A.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define endl "\n"
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    ll t; cin >> t;
+    while(t--){
+        ll a[3];
+        for(ll i=0; i<3; i++){
+            cin >> a[i];
+        }
+        sort(a,a+3);
+        if(a[0]+a[1]==a[2]){
+            cout << "YES" << endl;
+        }else{
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
