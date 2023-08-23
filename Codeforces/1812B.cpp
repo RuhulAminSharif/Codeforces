a15ec3720003318cb28e07b7bc5901c31ac9ce57
RuhulAminSharif <sharif.cse.ras@gmail.com>
2023-08-23 17:51:43 +0600

    1812B added

---
+++ Codeforces/1812B.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define endl "\n"
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int n; cin >> n;
+    if(n==15 || n==20 || n==21){
+        cout << "NO" << endl;
+    }else{
+        cout << "YES" << endl;
+    }
+    return 0;
+}
