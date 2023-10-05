8e0f98b82b6debce6504bbea788b43e4703f2d38
RuhulAminSharif <sharif.cse.ras@gmail.com>
2023-10-05 17:55:48 +0600

    (+)50A

---
+++ Codeforces/50A.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define endl "\n"
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    ll m, n; cin >> m >> n;
+    cout << (m*n)/2 << endl;
+    return 0;
+}
