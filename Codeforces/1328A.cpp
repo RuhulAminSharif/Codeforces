117a0a8b632a9aebcd1d27297d3aec67f8d1c41c
RuhulAminSharif <sharif.cse.ras@gmail.com>
2023-10-07 21:54:23 +0600

    (+)1328A

---
+++ Codeforces/1328A.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define endl "\n"
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    ll t; cin >> t;
+    while(t--){
+        ll a, b; cin >> a >> b;
+        if(a%b==0) cout << 0;
+        else cout << b - a%b ;
+        cout << endl;
+    }
+    return 0;
+}
