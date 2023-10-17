442ad971b987153bcea6d4e4b8e1bf917e64f3ff
RuhulAminSharif <sharif.cse.ras@gmail.com>
2023-10-17 22:28:06 +0600

    (+)35

---
+++ Leetcode/35.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int start=0, end=nums.size()-1, mid;
+        while(start<=end){
+            mid = start + (end-start)/2;
+            if(nums[mid]>=target){
+                end = mid-1;
+            }else{
+                start = mid+1;
+            }
+        }
+        return start;
+    }
+};
