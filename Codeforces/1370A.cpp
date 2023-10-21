7f4d48b0744240bc480ccc31ff419195b11d2687
RuhulAminSharif <sharif.cse.ras@gmail.com>
2023-10-21 14:59:58 +0600

    (+)1370A

---
+++ Codeforces/1370A.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define endl "\n"
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    ll t; cin >> t;
+    while(t--){
+        ll n; cin >> n;
+        cout << n/2 << endl;
+    }
+    return 0;
+}
