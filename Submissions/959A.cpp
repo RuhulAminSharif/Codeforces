ed4935cb690e25b8f8e6ac257354fcccf0ab310c
RuhulAminSharif <sharif.cse.ras@gmail.com>
2023-10-27 21:09:42 +0600

    (+)959A

---
+++ Submissions/959A.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define ll long long int
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int n; cin >> n;
+    if(n&1) cout << "Ehab" << endl;
+    else cout << "Mahmoud" << endl;
+    return 0;
+}
