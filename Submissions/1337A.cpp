ef1fbce7cdd95d5e83447d5d1e9a8f02bc2a1598
Ruhul Amin Sharif <sharif.cse.ras@gmail.com>
2024-01-02 19:33:04 +0600

    (+)1337A

---
+++ Submissions/1337A.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define endl "\n"
+const ll mx = 1e5+7;
+void solve(void)
+{
+    ll a, b, c, d; cin >> a >> b >> c >> d;
+    cout << b << " " << c << " " << c << endl;
+    return ;
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    ll t=1, test=0;
+    cin >> t;
+    while(t--){
+        /// cout << "Case : " << ++test << endl;
+        solve();
+    }
+
+    return 0;
+}
