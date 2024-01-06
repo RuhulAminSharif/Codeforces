56c69aa1bcf6e33d957740a78bd6d2e201be6ffb
Ruhul Amin Sharif <sharif.cse.ras@gmail.com>
2024-01-07 00:06:08 +0600

    (+)1919A

---
+++ Submissions/1919A.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define endl "\n"
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    ll t; cin >> t;
+    while(t--){
+        ll a, b; cin >> a >> b;
+        if(abs(a-b)&1)
+            cout << "Alice" << endl;
+        else
+            cout << "Bob" << endl;
+    }
+    return 0;
+}
