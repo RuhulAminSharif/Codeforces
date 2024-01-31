977d4504b782b86c276b47f2fe956b754f94bd22
Ruhul Amin Sharif <sharif.cse.ras@gmail.com>
2024-01-31 08:19:10 +0600

    (+)1918A

---
+++ Submissions/1918A.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define endl "\n"
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    ll t; cin >> t;
+    while(t--){
+        ll n, k; cin >> n >> k;
+        k >>= 1;
+        cout << n * k << endl;
+    }
+    return 0;
+}
